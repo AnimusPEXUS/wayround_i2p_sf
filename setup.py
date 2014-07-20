d9192539e0158b89ec364764e6e1113ccdec23ef
Alexey V Gorshkov <animus@wayround.org>
2014-07-21 00:10:35 +0400

    release

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+setup(
+    name='org_wayround_sf',
+    version='0.1',
+    description='walk() function for sf.net files',
+    author='Alexey Gorshkov',
+    author_email='animus@wayround.org',
+    url='http://wiki.wayround.org/soft/org_wayround_sf',
+    packages=[
+        'org.wayround.sf',
+        ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Development Status :: 1 - Planning'
+        ]
+    )
